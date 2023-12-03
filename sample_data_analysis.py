d4c65b6a167163a2805ff1de85f6364f69277a1d
adityaaher2004 <adityaaher2004@gmail.com>
2023-12-03 02:43:48 -0500

    Modified sample_data_analysis.py -> Generates Analysis of game_metadata.json -> Generates summary of games.csv
    Added games.csv, games_metadata.json

---
+++ sample_data_analysis.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+
+genre_occurrences = {}
+total_genres = 0
+total_occurrences = 0
+
+with open('games_metadata.json', 'r', encoding='utf-8') as file:
+    for line in file:
+        try:
+            data = json.loads(line)
+            tags = data["tags"]
+            name = data["app_id"]
+            for genre in tags:
+                if genre not in genre_occurrences:
+                    total_genres += 1
+                    genre_occurrences[genre] = 1
+                else:
+                    genre_occurrences[genre] += 1
+                total_occurrences += 1
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+
+print(f"Total Genres = ", total_genres)
+print(f"Total Occurrences = ", total_occurrences)
+
+filename = "games.csv"
+df = pd.read_csv(filename)
+print()
+print("Data Frame Information:")
+df_features = df.info()
+print(df_features)
