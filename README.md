7ec01b0089ddbbf4c83c79ff1c3e6f3d9c8b5c7b
adityaaher2004 <adityaaher2004@gmail.com>
2023-12-16 23:11:53 -0500

    Organised and cleaned repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# video-game-gnn
-Creating a sophisticated video game recommendation system using graph neural networks
---
+++ Scratches and Misc/scratch.py
@@ -0,0 +1,14 @@
+import json
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import torch
+
+# path = "data/games_processed.csv"
+# data = pd.read_csv(path)
+# for index, game in tqdm(data.iterrows(), total=data.shape[0]):
+#     print(index)
+#     print((game.array[0]))
+#     break
+
+print(torch.tensor(5))
